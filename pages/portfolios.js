4b89f250bc7314fe909e0e95e14c3fe9ddb0900b
marcinszablowski <marcin.szablowski@op.pl>
2021-03-10 22:56:07 +0100

    Fetch posts with Axios and display on Portfolios page

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 import BaseLayout from "../components/layouts/BaseLayout";
 
 class Portfolios extends React.Component {
+  //Fetch data to initial props using Axios
+  static async getInitialProps() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.data;
+
+    return {posts: data};
+  };
+
   render() {
+    const postsData = this.props.posts;
     return (
       <BaseLayout>
         <h1>Portfolios</h1>
+        <ul>
+          {postsData.map(post => (
+            <li key={post.id}>
+              {post.title}
+            </li>
+          ))}
+        </ul>
       </BaseLayout>
     );
   }
