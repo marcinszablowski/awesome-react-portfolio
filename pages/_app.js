3c54d49fb58e19ea5e8e0c6cd089a4cd907eac82
marcinszablowski <marcin.szablowski@op.pl>
2021-03-14 18:18:26 +0100

    Add Head and page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
 import '../styles/main.scss';
 
@@ -8,7 +9,7 @@ class MyApp extends React.Component {
   static async getInitialProps({Component, ctx}) {
     let pageProps = {};
     if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx);
+      pageProps = await Component.getInitialProps(ctx);
     }
 
     return {pageProps};
@@ -16,7 +17,15 @@ class MyApp extends React.Component {
 
   render() {
     const {Component, pageProps} = this.props;
-    return <Component {...pageProps} />
+    return (
+      <>
+        <Head>
+          <title>Next Portfolio</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
   }
 }
 
